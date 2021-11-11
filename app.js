ac00347f5e18774daa3fb93aa78da51510c83f3a
spun <reinaldobust97@gmail.com>
2021-11-11 13:30:02 -0300

    cors

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
-const cors = require("cors")
+
 const app = express()
 const port = process.env.PORT || 5000
 
-const config = require('./config');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use(cors(
-    config.application.cors.server
-));
+// FIX CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 // MYSQL
 
