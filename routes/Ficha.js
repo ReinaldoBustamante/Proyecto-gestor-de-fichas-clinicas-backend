179b326f65cd7dd3fd1b890e9586f5e2d0258a3b
spun <reinaldobust97@gmail.com>
2021-11-30 18:19:36 -0300

    post api  fecha_ficha, fecha_implementacion

--- routes/Ficha.js
+++ routes/Ficha.js
@@ -33,13 +33,15 @@ router.route("/fichas")
     })
     .post((req, res) => {
         const rut = req.body.rut
-       
+        const fecha_nacimiento = req.body.fecha_nacimiento
+        const fecha_ficha = req.body.fecha_ficha
+
         db.getConnection((err, connection) =>{
             if(err) throw err
             console.log(`conected as id ${connection.threadId}`)
     
-            connection.query('INSERT INTO ficha(rut) VALUES (?)',
-            [rut],
+            connection.query('INSERT INTO ficha(rut, fecha_nacimiento, fecha_ficha) VALUES (?, ?, ?)',
+            [rut, fecha_nacimiento,fecha_ficha],
             (err, rows) =>{
                 connection.release() // return the connection to pool
     
