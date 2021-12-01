c25920fa966e3e5e546df9e176c7124c03b60e1b
spun <reinaldobust97@gmail.com>
2021-12-01 13:57:25 -0300

    heroiku

--- connection.js
+++ connection.js
@@ -4,11 +4,11 @@ const mysql = require('mysql')
 // MYSQL
 
 const pool = mysql.createPool({
-    host            : 'fedent-api-do-user-10349940-0.b.db.ondigitalocean.com',
-    port            :  '25060',
-    user            : 'doadmin',
-    password        : 'ASiEm5OBBilhaLg5',
-    database        : 'defaultdb'
+    host            : 'yjo6uubt3u5c16az.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port            :  '3306',
+    user            : 'rvm60oxi1nj6xss7',
+    password        : 'w5slqddkaoo937bv',
+    database        : 'xdb7zi3os0mo4g8o'
     
 })
 
